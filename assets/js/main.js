6b3b682a5b9d98da55f13461ffbdfa2a86b41521
Felipe <fmp.ti@outlook.com>
2023-03-02 11:02:05 -0300

    Ajeitando a responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,12 @@ var btn_voltar = document.querySelector("#voltar");
 btn_voltar.addEventListener("click", function(){
     window.scrollTo(0, 0);
 });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
